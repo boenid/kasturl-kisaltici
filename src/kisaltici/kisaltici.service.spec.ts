f00b471bafcab39b6e3de95aebf341e14b5139fd
Enes Kamil Boğaz <enes.kamil.bogaz@yaani.com>
2025-12-26 11:13:53 +0300

    fix: eslint unsafe kurallari dosya bazında daevre dışı bırakıldı

--- src/kisaltici/kisaltici.service.spec.ts
+++ src/kisaltici/kisaltici.service.spec.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/unbound-method */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { KisalticiService } from './kisaltici.service';
